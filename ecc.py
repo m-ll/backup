281d0969b59922ef97fabdc3fc0c78c8aa8d8d2b
m-ll <43573326+m-ll@users.noreply.github.com>
2019-01-18 10:33:05 +0100

    feat: enable directory input for ecc

--- ecc.py
+++ ecc.py
@@ -11,42 +11,16 @@
 
 import argparse
 import datetime
+import glob
 import os
+import re
 import sys
 sys.path.append( os.path.join( os.path.dirname( __file__ ), 'unireedsolomon' ) )
+sys.path.append( os.path.join( os.path.dirname( __file__ ), 'colorama' ) )
 
+from colorama import init, Fore
+init( autoreset=True )
 import unireedsolomon
-
-parser = argparse.ArgumentParser()
-parser.add_argument( 'action', choices=['create', 'check', 'fix'], help='The action to process' )
-parser.add_argument( '-i', '--input', required=True, help='The input file' )
-parser.add_argument( '-e', '--ecc', required=True, help='The corresponding ecc file' )
-parser.add_argument( '-f', '--fix', help='The fixed output file (action=fix)' )
-args = parser.parse_args()
-
-if args.action == 'fix' and args.fix is None:
-	parser.print_help( sys.stderr )
-	sys.exit( 1 )
-
-if not os.path.isfile( args.input ):
-	print( 'The input file doesn\'t exist: {}'.format( args.input ), file=sys.stderr )
-	sys.exit( 1 )
-
-if args.action == 'create':
-	if os.path.isfile( args.ecc ):
-		print( 'An ecc file already exists: {}'.format( args.ecc ), file=sys.stderr )
-		sys.exit( 1 )
-elif args.action == 'check':
-	if not os.path.isfile( args.ecc ):
-		print( 'An ecc file doesn\'t exists: {}'.format( args.ecc ), file=sys.stderr )
-		sys.exit( 1 )
-elif args.action == 'fix':
-	if not os.path.isfile( args.ecc ):
-		print( 'An ecc file doesn\'t exists: {}'.format( args.ecc ), file=sys.stderr )
-		sys.exit( 1 )
-	elif os.path.isfile( args.fix ):
-		print( 'A fix file already exists: {}'.format( args.fix ), file=sys.stderr )
-		sys.exit( 1 )
 
 #---
 
@@ -54,9 +28,18 @@ def now():
 	return datetime.datetime.now().time()
 
 def print_progress( iCurrent, iTotal ):
-	print( '  processed: {:.2f}%'.format( iCurrent / iTotal * 100.0 ), end='\r' )
+	print( '[{}]   processed: {:.2f}%'.format( now(), iCurrent / iTotal * 100.0 ), end='\r' )
 
 #---
+
+parser = argparse.ArgumentParser()
+parser.add_argument( 'action', choices=['create', 'check', 'fix'], help='The action to process' )
+parser.add_argument( '-i', '--input', required=True, help='The input file' )
+args = parser.parse_args()
+
+#---
+
+# Initialize after argparse, as it would be possible to get values as parameters
 
 # for 1 Mo:
 #
@@ -78,89 +61,133 @@ prim = 0x11b
 # prim = unireedsolomon.ff.find_prime_polynomials( c_exp=exp, fast_primes=True, single=True )
 coder = unireedsolomon.rs.RSCoder( result_size, message_size, prim=prim, c_exp=exp )
 
-src_name = args.input
-ecc_name = args.ecc
+#---
 
-size_to_process = os.path.getsize( src_name )
+pathfiles = []
 
-if args.action == 'create':
+if os.path.isfile( args.input ):
+	pathfiles.append( { 
+						'input': args.input, 
+						'ecc': f'{args.input}.ecc-{result_size}-{message_size}-{exp}', 
+						'fix': f'{args.input}.regenerated'
+					  } )
+elif os.path.isdir( args.input ):
+	root = args.input
+	for entry in glob.iglob( root + '**/.*', recursive=True ):
+		if re.match( r'.+\.regenerated$', entry ) or re.match( r'.+\.ecc-.+-.+', entry ):
+			continue
+		if not os.path.isfile( entry ):
+			continue
+		pathfiles.append( { 
+							'input': entry, 
+							'ecc': f'{entry}.ecc-{result_size}-{message_size}-{exp}', 
+							'fix': f'{entry}.regenerated'
+						} )
+	for entry in glob.iglob( root + '**/*', recursive=True ):
+		if re.match( r'.+\.regenerated$', entry ) or re.match( r'.+\.ecc-.+-.+', entry ):
+			continue
+		if not os.path.isfile( entry ):
+			continue
+		pathfiles.append( { 
+							'input': entry, 
+							'ecc': f'{entry}.ecc-{result_size}-{message_size}-{exp}', 
+							'fix': f'{entry}.regenerated'
+						} )
+else:
+	print( Fore.RED + 'The input file doesn\'t exist: {}'.format( args.input ) )
+	sys.exit( 1 )
 
-	print( '[{}] {} -> {}'.format( now(), src_name, ecc_name ) )
-	print( '[{}] begin create ...'.format( now() ) )
+#---
 
-	# with open( src_name, 'rb' ) as src, open( dst_name, 'wb' ) as dst, open( ecc_name, 'wb' ) as ecc:
-	with open( src_name, 'rb' ) as src, open( ecc_name, 'wb' ) as ecc:
-		for chunk in iter( lambda: src.read( message_size ), b'' ):
-			padding = message_size - len( chunk )
-			if padding:
-				chunk = b'\0' * padding + chunk
+for pathfile in pathfiles:
 
-			c = coder.encode_fast( chunk, return_string=False )
-			
-			# If using exp > 8 -> value may be > 255 -> can't store them inside 1 byte
-			# b = bytearray()
-			# for n in c:
-				# b.extend( n.to_bytes( ( n.bit_length() + 7 ) // 8, byteorder='little' ) )
-			# # dst.write( b )
+	src_name = pathfile['input']
+	ecc_name = pathfile['ecc']
+	fix_name = pathfile['fix']
 
-			b = bytes( c )
-			# dst.write( b[padding:message_size] )
-			ecc.write( b[message_size:] )
+	size_to_process = os.path.getsize( src_name )
 
-			print_progress( src.tell(), size_to_process )
-		print() # To go to next line after previous \r
+	if args.action == 'create':
+		if os.path.isfile( ecc_name ):
+			print( Fore.CYAN + '  Skip: ecc file already exists: {}'.format( ecc_name ) )
+			continue
+	elif args.action == 'check':
+		if not os.path.isfile( ecc_name ):
+			print( Fore.CYAN + '  Skip: ecc file doesn\'t exists: {}'.format( ecc_name ) )
+			continue
+	elif args.action == 'fix':
+		if not os.path.isfile( ecc_name ):
+			print( Fore.CYAN + '  Skip: ecc file doesn\'t exists: {}'.format( ecc_name ) )
+			continue
 
-	print( '[{}] end create'.format( now() ) )
+	#---
 
-elif args.action == 'check':
+	if args.action == 'create':
 
-	print( '[{}] {} + {}'.format( now(), src_name, ecc_name ) )
-	print( '[{}] begin check ...'.format( now() ) )
+		print( '[{}] {} -> {}'.format( now(), src_name, ecc_name ) )
 
-	with open( src_name, 'rb' ) as src, open( ecc_name, 'rb' ) as ecc:
-		for src_chunk, ecc_chunk in zip( iter( lambda: src.read( message_size ), b'' ), iter( lambda: ecc.read( errors ), b'' ) ):
-			padding = message_size - len( src_chunk )
-			if padding:
-				src_chunk = b'\0' * padding + src_chunk
-			chunk = src_chunk + ecc_chunk
+		# with open( src_name, 'rb' ) as src, open( dst_name, 'wb' ) as dst, open( ecc_name, 'wb' ) as ecc:
+		with open( src_name, 'rb' ) as src, open( ecc_name, 'wb' ) as ecc:
+			for chunk in iter( lambda: src.read( message_size ), b'' ):
+				padding = message_size - len( chunk )
+				if padding:
+					chunk = b'\0' * padding + chunk
 
-			if args.action == 'check':
+				c = coder.encode_fast( chunk, return_string=False )
+				
+				# If using exp > 8 -> value may be > 255 -> can't store them inside 1 byte
+				# b = bytearray()
+				# for n in c:
+					# b.extend( n.to_bytes( ( n.bit_length() + 7 ) // 8, byteorder='little' ) )
+				# # dst.write( b )
+
+				b = bytes( c )
+				# dst.write( b[padding:message_size] )
+				ecc.write( b[message_size:] )
+
+				print_progress( src.tell(), size_to_process )
+			print() # To go to next line after previous \r
+
+	elif args.action == 'check':
+
+		print( '[{}] {} + {}'.format( now(), src_name, ecc_name ) )
+
+		with open( src_name, 'rb' ) as src, open( ecc_name, 'rb' ) as ecc:
+			for src_chunk, ecc_chunk in zip( iter( lambda: src.read( message_size ), b'' ), iter( lambda: ecc.read( errors ), b'' ) ):
+				padding = message_size - len( src_chunk )
+				if padding:
+					src_chunk = b'\0' * padding + src_chunk
+				chunk = src_chunk + ecc_chunk
+
 				check = coder.check_fast( chunk )
 				if not check:
 					print()
-					print( 'There are errors -_-' )
-					sys.exit( 10 )
-					
-			print_progress( src.tell(), size_to_process )
-		print()
+					print( Fore.RED + 'There are errors -_-' )
+						
+				print_progress( src.tell(), size_to_process )
+			print()
+			
+	elif args.action == 'fix':
+		
+		print( '[{}] {} + {} -> {}'.format( now(), src_name, ecc_name, fix_name ) )
 
-	print( '[{}] end check'.format( now() ) )
+		with open( src_name, 'rb' ) as src, open( ecc_name, 'rb' ) as ecc, open( fix_name, 'wb' ) as fix:
+			for src_chunk, ecc_chunk in zip( iter( lambda: src.read( message_size ), b'' ), iter( lambda: ecc.read( errors ), b'' ) ):
+				padding = message_size - len( src_chunk )
+				if padding:
+					src_chunk = b'\0' * padding + src_chunk
+				chunk = src_chunk + ecc_chunk
+
+				d, code = coder.decode_fast( chunk, nostrip=True, return_string=False )
+
+				b = bytes( d )
+				fix.write( b[padding:] )
+
+				print_progress( src.tell(), size_to_process )
+			print()
+
+if args.action == 'check':
 	print()
 	print( '(The result is not foolproof:' )
 	print( '(if it’s False, you’re sure the message was corrupted (or that you used the wrong RS parameters),' )
 	print( '(if it’s True, it’s either that the message is correct, or that there are too many errors)' )
-
-elif args.action == 'fix':
-	
-	fix_name = args.fix
-
-	print( '[{}] {} + {} -> {}'.format( now(), src_name, ecc_name, fix_name ) )
-	print( '[{}] begin fix ...'.format( now() ) )
-
-	with open( src_name, 'rb' ) as src, open( ecc_name, 'rb' ) as ecc, open( fix_name, 'wb' ) as fix:
-		for src_chunk, ecc_chunk in zip( iter( lambda: src.read( message_size ), b'' ), iter( lambda: ecc.read( errors ), b'' ) ):
-			padding = message_size - len( src_chunk )
-			if padding:
-				src_chunk = b'\0' * padding + src_chunk
-			chunk = src_chunk + ecc_chunk
-
-			d, code = coder.decode_fast( chunk, nostrip=True, return_string=False )
-
-			b = bytes( d )
-			fix.write( b[padding:] )
-
-			print_progress( src.tell(), size_to_process )
-		print()
-
-	print( '[{}] end fix'.format( now() ) )
-
